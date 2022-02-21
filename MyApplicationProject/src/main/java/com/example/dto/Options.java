a0ec1f8295785f1c5690d2bf416094588e21fb09
Ajay <panchalajay89@gmail.com>
2022-02-21 17:04:13 +0530

    issue Resolved

--- MyApplicationProject/src/main/java/com/example/dto/Options.java
+++ MyApplicationProject/src/main/java/com/example/dto/Options.java
@@ -3,13 +3,13 @@ package com.example.dto;
 public class Options {
 
 	
-	private String Label_Date;
+	private String label_Date;
 	
-	private String Saturday_Delivery;
+	private String saturday_Delivery;
 	
-	private String Dry_Ice_Wt;
+	private String dry_Ice_Wt;
 	
-	private String Label_Img;
+	private String label_Img;
 
 	public Options() {
 	
@@ -17,53 +17,50 @@ public class Options {
 
 	public Options(String label_Date, String saturday_Delivery, String dry_Ice_Wt, String label_Img) {
 		super();
-		Label_Date = label_Date;
-		Saturday_Delivery = saturday_Delivery;
-		Dry_Ice_Wt = dry_Ice_Wt;
-		Label_Img = label_Img;
+		this.label_Date = label_Date;
+		this.saturday_Delivery = saturday_Delivery;
+		this.dry_Ice_Wt = dry_Ice_Wt;
+		this.label_Img = label_Img;
 	}
 
 	public String getLabel_Date() {
-		return Label_Date;
+		return label_Date;
 	}
 
 	public void setLabel_Date(String label_Date) {
-		Label_Date = label_Date;
+		this.label_Date = label_Date;
 	}
 
 	public String getSaturday_Delivery() {
-		return Saturday_Delivery;
+		return saturday_Delivery;
 	}
 
 	public void setSaturday_Delivery(String saturday_Delivery) {
-		Saturday_Delivery = saturday_Delivery;
+		this.saturday_Delivery = saturday_Delivery;
 	}
 
 	public String getDry_Ice_Wt() {
-		return Dry_Ice_Wt;
+		return dry_Ice_Wt;
 	}
 
 	public void setDry_Ice_Wt(String dry_Ice_Wt) {
-		Dry_Ice_Wt = dry_Ice_Wt;
+		this.dry_Ice_Wt = dry_Ice_Wt;
 	}
 
 	public String getLabel_Img() {
-		return Label_Img;
+		return label_Img;
 	}
 
 	public void setLabel_Img(String label_Img) {
-		Label_Img = label_Img;
+		this.label_Img = label_Img;
 	}
 
 	@Override
 	public String toString() {
-		return "Options [Label_Date=" + Label_Date + ", Saturday_Delivery=" + Saturday_Delivery + ", Dry_Ice_Wt="
-				+ Dry_Ice_Wt + ", Label_Img=" + Label_Img + "]";
+		return "Options [label_Date=" + label_Date + ", saturday_Delivery=" + saturday_Delivery + ", dry_Ice_Wt="
+				+ dry_Ice_Wt + ", label_Img=" + label_Img + "]";
 	}
 
-	
-	
-	
 	
 	
 
